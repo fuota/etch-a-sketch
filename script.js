aa2738632bb1e0636691ad11c89922b752a250e4
Tam Do <phuongtam1072@gmail.com>
2022-06-03 23:24:51 +0700

    Create color choice function

--- script.js
+++ script.js
@@ -26,3 +26,46 @@ sizeButtons.forEach((btn) => { btn.addEventListener("click", function() {
     container.style.gridTemplateColumns = `repeat(${btn.value}, auto) `;    
     container.style.gridTemplateRows = `repeat(${btn.value}, auto) `
 })})
+
+const colorButtons = document.querySelectorAll(".color")
+const warmColors = ["#ffb8b8", "#ffc0ab", "#ffcd9a", "#ffd6ab", "#fff7a9"]
+const coldColors = ["#4B77B5", "#799FCC", "#AFC6D0", "#153576", "#153576"]
+colorButtons.forEach((button) => {button.addEventListener("click", function(){
+    const items = document.querySelectorAll(".item");
+    deactive(colorButtons);
+    button.classList.add("active");
+
+    //Warm Color
+    if (button.classList.contains("warm")) {
+        items.forEach((item) => {item.addEventListener("mouseover", function() {
+            item.style.backgroundColor = warmColors.randomchoice()
+
+        } ) })
+    }
+    //Cold Color
+
+    if (button.classList.contains("cold")) {
+        items.forEach((item) => {item.addEventListener("mouseover", function() {
+            item.style.backgroundColor = coldColors.randomchoice()
+
+        } ) })
+    }
+    //White Color
+
+    if (button.classList.contains("white")) {
+        items.forEach((item) => {item.addEventListener("mouseover", function() {
+            item.style.backgroundColor = "white"
+
+        } ) })
+    }
+    //Black Color
+
+    if (button.classList.contains("black")) {
+        items.forEach((item) => {item.addEventListener("mouseover", function() {
+            item.style.backgroundColor = "black"
+
+        } ) })
+    }
+})
+}
+)
