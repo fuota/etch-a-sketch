f6572cd39d63aff857ecb31ef2edb842f991018b
Tam Do <phuongtam1072@gmail.com>
2022-06-03 23:22:09 +0700

    Create generate grid and deactive style function

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ Array.prototype.randomchoice = function(){
     return this[Math.floor(Math.random()*this.length)];
   }
 
+  function deactive(buttons) {
+    buttons.forEach((button) => {
+        button.classList.remove('active');
+    });
+}
+
+sizeButtons.forEach((btn) => { btn.addEventListener("click", function() {
+    deactive(sizeButtons);
+    deactive(colorButtons);
+    btn.classList.add("active");
+    container.innerHTML="";
+    for (let i=1; i<=(btn.value*btn.value); i+=1) {
+        let item = document.createElement("div");
+        item.classList.add("item");
+        container.appendChild(item);
+    };
+    container.style.gridTemplateColumns = `repeat(${btn.value}, auto) `;    
+    container.style.gridTemplateRows = `repeat(${btn.value}, auto) `
+})})
